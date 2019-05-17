b0fd9d407d146a3021870438a60e663ef7cb3610
Oscar Andersson <oscar.andersson3@ikea.com>
2019-05-18 00:09:50 +0200

    weather done

--- js/index.js
+++ js/index.js
@@ -19,14 +19,18 @@ $(function () {
 });
 
 function showPosition(pos) {
-    let url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + pos.coords.latitude + '&lon=' + pos.coords.longitude + '&appid=' + weatherKey;
+    let url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + pos.coords.latitude + '&lon=' + pos.coords.longitude + '&units=metric' + '&appid=' + weatherKey;
 
     var xhr = new XMLHttpRequest();
 
     xhr.onload = function () {
         if (xhr.status >= 200 && xhr.status < 300) {
-            console.log('success!', xhr);
-
+            console.log('success!', xhr.response);
+            let weather = JSON.parse(xhr.response);
+            console.log('weather in ' + weather.name + ' is ' + weather.main.temp);
+            $('.city-name').text(weather.name);
+            $('.temp-text').html("<i class='owf owf-" + weather.weather[0].id +"'></i> " + Math.round(weather.main.temp) + '°C');
+            $('.temp-desc').text(weather.weather[0].description);
         } else {
             console.log('The request failed!');
         }
